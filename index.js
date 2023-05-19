1f212d8c5f2c39e2ee1307f77bbb9bf44815ee5a
saadrafi <saadrafi81@gmail.com>
2023-05-20 04:16:07 +0600

    delete toy data function added

--- index.js
+++ index.js
@@ -85,6 +85,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete toy data by id
+    app.delete("/deletetoy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
